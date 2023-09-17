f6da8be00c917f72888b3d0022a4f8d429a7d69e
Ekin Aslan <ekinaslan.js@gmail.com>
2023-09-17 15:54:14 +0300

    Fix

--- register/register.php
+++ register/register.php
@@ -3,25 +3,50 @@
 require "../utilities/post.php";
 require "../utilities/database.php";
 
-$email = post()["email"];
+$registry = post();
+
+switch ($registry["phase"]) {
+    case "register":
+        register($registry["email"]);
+        break;
+    case "confirm":
+        confirm();
+        break;
+    case "create":
+        create();
+        break;
+}
+
+
+function register($email)
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (register_email_control($email)) {
+            session_start();
+            $_SESSION["email"] = $email;
+            $_SESSION["code"] = mt_rand(10000, 99999);
+            echo json_encode(["status" => "success", "NOTFROMHERE" => $_SESSION["code"]]);
+        } else
+            echo json_encode(["status" => "email_used"]);
+    } else
+        echo json_encode(["status" => "email_invalid"]);
+}
+
+function confirm()
+{
+    echo json_encode(["status" => "nope"]);
+}
+
+function create()
+{
+    echo json_encode(["status" => "nope"]);
+}
 
 
 
 
 
 
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    if (register_email_check($email)) {
 
-        session_start();
-
-        $_SESSION["email"] = $email;
-
-        echo json_encode(["status" => "success"]);
-    } else {
-        echo json_encode(["status" => "email_used"]);
-    }
-} else
-    echo json_encode(["status" => "email_invalid"]);
 
 ?>
