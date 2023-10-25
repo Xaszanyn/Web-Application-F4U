e44738d8a05203abc8f5a5ff94831153230f189b
Ekin Aslan <ekinaslan.js@gmail.com>
2023-10-25 19:06:57 +0300

    Menus Final

--- menus.php
+++ menus.php
@@ -5,11 +5,6 @@ require "./utilities/database.php";
 if ($_SERVER["REQUEST_METHOD"] != "GET")
     die("Hata");
 
-
-$menus = get_menus();
-
-print_r($menus);
-
-
+echo json_encode(get_menus());
 
 ?>
