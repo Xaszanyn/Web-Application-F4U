b09334591b6acb2aab60ff53eacf185469133d18
Ekin Aslan <ekinaslan.js@gmail.com>
2024-02-05 17:10:25 +0300

    Fix

--- payment.php
+++ payment.php
@@ -1,14 +1,12 @@
 <?php
 
-echo "test p";
+require "./utilities/post.php";
 
-// require "./utilities/post.php";
+$request = post();
 
-// $request = post();
-
-// $file = fopen("output.txt", "w");
-// fwrite($file, implode(", ", array_keys($request)) . "\n");
-// fwrite($file, implode(", ", $request));
-// fclose($file);
+$file = fopen("OUTPUT   .txt", "w");
+fwrite($file, implode(", ", array_keys($request)) . "\n");
+fwrite($file, implode(", ", $request));
+fclose($file);
 
 ?>
