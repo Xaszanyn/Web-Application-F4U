e7c5ebd2f6a0cd772b34baa5a9a966694d230826
Ekin Aslan <ekinaslan.js@gmail.com>
2024-02-21 17:34:48 +0300

    Fix

--- utilities/database.php
+++ utilities/database.php
@@ -210,7 +210,7 @@ function create_order_request($menu_id, $province_id, $district_id, $days, $time
 
     $connection = connect();
 
-    $query = "INSERT INTO order_requests(menu_id, province_id, district_id, days, time, promotion, name, phone, email, address, height, weight, allergy, disease, occupation, extra) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $query = "INSERT INTO order_requests(menu_id, date, province_id, district_id, days, time, promotion, name, phone, email, address, height, weight, allergy, disease, occupation, extra) VALUES (?, CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $result = mysqli_prepare($connection, $query);
     $allergy = empty($allergy) ? "-" : $allergy;
     $disease = empty($disease) ? "-" : $disease;
