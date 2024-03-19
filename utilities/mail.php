c2f986d4beacbd3340ee11eaa03dbd8f6b9e9c4f
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-20 01:55:52 +0300

    Test

--- utilities/mail.php
+++ utilities/mail.php
@@ -2,10 +2,14 @@
 
 // mail("ekinaslant@gmail.com", "Fit4U", "aa numaralı siparişinizin ödemesi başarıyla alınmıştır. İyi günler dileriz.");
 
-mail(
-    "ekinaslant@gmail.com",
-    'the subject',
-    'the message',
-    null,
-    '-fnoreply@fitgelsin.com'
-);
+// // mail(
+// //     "ekinaslant@gmail.com",
+// //     'the subject',
+// //     'the message',
+// //     null,
+// //     '-fnoreply@fitgelsin.com'
+// // );
+
+mail('ekinaslant@gmail.com', 'Subject Line Here', 'Body of Message Here', 'From: noreply@fitgelsin.com');
+
+echo "test";
