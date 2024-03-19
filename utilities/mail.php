aedb59190fa8cf003823ebabdfe33613f2b9b860
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-20 02:26:36 +0300

    Test

--- utilities/mail.php
+++ utilities/mail.php
@@ -1,15 +1,40 @@
 <?php
 
-// mail("ekinaslant@gmail.com", "Fit4U", "aa numaralı siparişinizin ödemesi başarıyla alınmıştır. İyi günler dileriz.");
+function send_mail($subject, $message)
+{
+    mail("ekinaslant@gmail.com", $subject, $message, [
+        "From" => "Fit Gelsin <no-reply@fitgelsin.com>",
+        "MIME-Version" => "1.0",
+        "Content-Type" => "text/html; charset=iso-8859-1"
+    ]);
+}
 
-// // mail(
-// //     "ekinaslant@gmail.com",
-// //     'the subject',
-// //     'the message',
-// //     null,
-// //     '-fnoreply@fitgelsin.com'
-// // );
 
-mail('ekinaslant@gmail.com', 'Subject Line Here', 'Body of Message Here', 'From: noreply@fitgelsin.com');
+
+send_mail("sub", "mes");
 
 echo "test";
+
+
+
+// use PHPMailer\PHPMailer\PHPMailer;
+
+
+// $mail = new PHPMailer(true);
+// $mail->isSMTP();
+// $mail->Host = "fitgelsin.com";
+// $mail->SMTPAuth = true;
+// $mail->Username = "noreply@fitgelsin.com";
+// $mail->Password = "C91YaYS(V&;~";
+// $mail->SMTPSecure = 'ssl';
+// $mail->Port = 465;
+// $mail->setFrom("noreply@fitgelsin.com", "Fit Gelsin");
+// $mail->addAddress("ekinaslant@gmail.com");
+// $mail->isHTML(true);
+// $mail->Subject = 'Test Email';
+// $mail->Body = 'This is a test email sent via PHPMailer';
+// $mail->send();
+
+// echo "test2";
+
+?>
