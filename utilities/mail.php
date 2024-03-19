3c38a2ada4825b34efd79850e323f4914b168993
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-20 01:48:25 +0300

    Test

--- utilities/mail.php
+++ utilities/mail.php
@@ -1,3 +1,11 @@
 <?php
 
-mail("ekinaslant@gmail.com", "Fit4U", "aa numaralı siparişinizin ödemesi başarıyla alınmıştır. İyi günler dileriz.");
+// mail("ekinaslant@gmail.com", "Fit4U", "aa numaralı siparişinizin ödemesi başarıyla alınmıştır. İyi günler dileriz.");
+
+mail(
+    "ekinaslant@gmail.com",
+    'the subject',
+    'the message',
+    null,
+    '-fnoreply@fitgelsin.com'
+);
