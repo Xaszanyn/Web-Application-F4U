f8ea90f76abe8a21bb9040b403e23dc453896f60
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-20 05:05:03 +0300

    :/

--- utilities/mail.php
+++ utilities/mail.php
@@ -8,7 +8,7 @@ function send_mail($target, $subject, $message, $name = "")
     str_replace('${message}', $message, $mail);
 
     mail($target, $subject, $mail, [
-        "From" => "Fit Gelsin <no-reply@fitgelsin.com>",
+        "From" => "Fit Gelsin", // <no-reply@fitgelsin.com>
         "MIME-Version" => "1.0",
         "Content-Type" => "text/html; charset=iso-8859-1"
     ]);
