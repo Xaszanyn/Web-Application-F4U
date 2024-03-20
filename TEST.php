bd4b9bb6d0097c95f49999cd1580d516f054cc5a
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-20 04:57:51 +0300

    Test

---
+++ TEST.php
@@ -0,0 +1,5 @@
+<?php
+
+require "./utilities/mail.php";
+
+send_mail("ekinaslant@gmail.com", "Subject", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum obcaecati unde quas, quam ipsa debitis expedita sint distinctio illo?", "Ekin");
