18c19e7086eee3dab28c3ac04617fb0f75ae4651
Ekin Aslan <ekinaslan.js@gmail.com>
2024-03-21 05:21:44 +0300

    Test

--- utilities/mail.php
+++ utilities/mail.php
@@ -7,9 +7,14 @@ function send_mail($target, $subject, $message, $name = "")
     str_replace('${name}', empty ($name) ? "" : " " . $name, $mail);
     str_replace('${message}', $message, $mail);
 
+    echo $mail;
+
     mail($target, $subject, $mail, [
         "From" => "Fit Gelsin <no-reply@fitgelsin.com>",
         "MIME-Version" => "1.0",
         "Content-Type" => "text/html; charset=iso-8859-1"
     ]);
 }
+
+
+send_mail("e001010011100101110111@gmail.com", "TEST MAIL", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum obcaecati unde quas, quam ipsa debitis expedita sint distinctio illo?", "Ekin");
