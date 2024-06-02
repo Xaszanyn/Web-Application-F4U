bb5d2479fb653ccef48ec4afa2f196f274cca7f9
Ekin Aslan <ekinaslan.js@gmail.com>
2024-06-02 22:03:10 +0300

    Test

--- utilities/mail.php
+++ utilities/mail.php
@@ -24,6 +24,8 @@ function send_order_mail($target, $subject, $message, $order)
     $mail = str_replace('${name}', empty($order["name"]) ? "" : " " . $order["name"], $mail);
     $mail = str_replace('${message}', $message, $mail);
 
+    echo $mail;
+
     $order = "<p>İsim:</p><p>{$order["name"]}</p><p>Telefon:</p><p>{$order["phone"]}</p><p>Menü:</p><p>{$order["menu_name"]}</p><p>Sipariş Tarihi:</p><p>{$order["date"]}</p><p>Gün Sayısı:</p><p>{$order["days"]}</p><p>Saat Aralığı:</p><p>{$order["time"]}</p><p>Adet:</p><p>{$order["amount"]}</p><p>İl:</p><p>{$order["province"]}</p><p>İlçe</p><p>{$order["district"]}</p><p>Adres:</p><p>{$order["address"]}</p>";
     $mail = str_replace('${order}', $order, $mail);
 
