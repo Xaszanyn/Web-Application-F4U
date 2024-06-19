b94e4e31a4172b31966468d32efe96500a4167e4
Ekin Aslan <ekinaslan.js@gmail.com>
2024-06-19 14:23:03 +0300

    Fix

--- utilities/configuration.php
+++ utilities/configuration.php
@@ -3,7 +3,7 @@
 define("DATABASE_HOST", "localhost");
 define("DATABASE_USERNAME", "Ekin");
 define("DATABASE_PASSWORD", "Rg%pqnmF&aUl");
-define("DATABASE", "F4U_Test");
+define("DATABASE", "F4U");
 
 define("PAYMES_URL", "https://api.paym.es/v4.6/order_create");
 define("PAYMES_PUBLIC_KEY", "9291d957-8df4-47b0-9ae5-b618da00ae29");
