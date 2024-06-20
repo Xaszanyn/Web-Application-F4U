756a9ce159fb79ebb3f339ed7f22856d139986d1
Ekin Aslan <ekinaslan.js@gmail.com>
2024-06-20 19:43:54 +0300

    Fix

--- TEST.php
+++ TEST.php
@@ -3,8 +3,27 @@
 require "./utilities/mail.php";
 
 
-send_announcement_mail("ekinduranov@gmail.com", "Fit Gelsin Doğrulama Kodu", "Fit Gelsin üyeliği için doğrulama kodunuz: <b>123456</b>");
+// send_announcement_mail("ekinduranov@gmail.com", "Fit Gelsin Doğrulama Kodu", "Fit Gelsin üyeliği için doğrulama kodunuz: <b>123456</b>");
+
+
+$order = [
+
+    "name" => "Name",
+    "phone" => "Phone",
+    "menu_name" => "Menu Name",
+    "date" => "Date",
+    "days" => "Days",
+    "time" => "Time 12345 678",
+    "amount" => "Amount",
+    "province" => "Province",
+    "district" => "District",
+    "address" => "Address ABCDE FGH 12345 678 90"
+
+
+];
+
+send_order_mail("ekinaslant@gmail.com", "Fit Gelsin Sipariş", "Siparişiniz başarıyla alınmıştır, sipariş ile ilgili detaylı bilgileri aşağıdan görüntüleyebilirsiniz. Faturanız kayıtlı e-posta adresinize iletilecektir.", $order);
+
+
 
 echo "TAMAM";
-
-// send_mail_text("e001010011100101110111@gmail.com", "Doğrulama Kodu", "Üyelik için doğrulama kodunuz <b>" . "12345" . "</b>.");
