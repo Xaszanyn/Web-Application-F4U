f5340108558ebdd6d357a7063badce45c700ee3a
Ekin Aslan <ekinaslan.js@gmail.com>
2024-06-20 20:21:26 +0300

    Fix

--- TEST.php
+++ TEST.php
@@ -3,26 +3,26 @@
 require "./utilities/mail.php";
 
 
-// send_announcement_mail("ekinduranov@gmail.com", "Fit Gelsin Doğrulama Kodu", "Fit Gelsin üyeliği için doğrulama kodunuz: <b>123456</b>");
+send_announcement_mail("ekinduranov@gmail.com", "Fit Gelsin Doğrulama Kodu", "Fit Gelsin üyeliği için doğrulama kodunuz: <b>123456</b>");
 
 
-$order = [
+// $order = [
 
-    "name" => "Name",
-    "phone" => "Phone",
-    "menu_name" => "Menu Name",
-    "date" => "Date",
-    "days" => "Days",
-    "time" => "Time 12345 678",
-    "amount" => "Amount",
-    "province" => "Province",
-    "district" => "District",
-    "address" => "Address ABCDE FGH 12345 678 90"
+//     "name" => "Name",
+//     "phone" => "Phone",
+//     "menu_name" => "Menu Name",
+//     "date" => "Date",
+//     "days" => "Days",
+//     "time" => "Time 12345 678",
+//     "amount" => "Amount",
+//     "province" => "Province",
+//     "district" => "District",
+//     "address" => "Address ABCDE FGH 12345 678 90"
 
 
-];
+// ];
 
-send_order_mail("ekinaslant@gmail.com", "Fit Gelsin Sipariş", "Siparişiniz başarıyla alınmıştır, sipariş ile ilgili detaylı bilgileri aşağıdan görüntüleyebilirsiniz. Faturanız kayıtlı e-posta adresinize iletilecektir.", $order);
+// send_order_mail("ekinaslant@gmail.com", "Fit Gelsin Sipariş", "Siparişiniz başarıyla alınmıştır, sipariş ile ilgili detaylı bilgileri aşağıdan görüntüleyebilirsiniz. Faturanız kayıtlı e-posta adresinize iletilecektir.", $order);
 
 
 
